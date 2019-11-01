7b4db68078949d1caefe4c90265af4ac8134f051
jasmine khalimova <jasmine.khalimova@transcore.ca>
2019-11-01 09:35:12 -0400

    added js cdn

--- js/main.js
+++ js/main.js
@@ -1,27 +1,30 @@
 //Validating The Product Title characters
-  function checkForm() {
-    var err = 0;
-  if (document.getElementById("product_title_input").value.length < 8) {
-      
-  document.getElementById("product_title_input").style.border = "1px solid #ECAA0B";
-  document.getElementById("text_err").innerHTML = "Minumum of 8 and Maximum of 100 Characters";
 
-  err = 1;
-  }
-    if ( err == 1 ) {
-       document.getElementById('next_button').setAttribute("disabled","disabled");
-       document.getElementById('next_button').style.background = "red";
-    } else {
-       document.getElementById('next_button').removeAttribute("disabled");
-       document.getElementById('next_button').style.background = "#76B62E";
+  var $input = $('input[type="text"]');
+
+var $button = $('input[type="button"]');
+
+setInterval(function(){
+
+    if($input.val().length < 8){
+
+        $button.attr('disabled', true);
+
+       $button.css('background-color', "red");
+
+    }else{
+
+        $button.attr('disabled', false);
+
+          $button.css('background-color', "green");
+
+          $button.css('color', "white");
+
     }
-  }
-  function setBorder(id) {
-  document.getElementById(id).style.border = "1px solid #ECAA0B";
-  document.getElementById(id+"_err").innerHTML = "";
-  }
-  document.getElementById('next_button').addEventListener("mouseover", checkForm);
 
+   
+
+}, 100);
 
 // Scroll Down Function
 $(function () {
